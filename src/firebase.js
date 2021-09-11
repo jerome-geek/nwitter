1fc3b9bc910c4ea67c141a2c4843d71c05e4c64c
Jerome <jerome@geekstudio.co.kr>
2021-09-11 12:35:36 +0900

    Add Firebase Set up

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBXdrZVy-892J9-ENXRJFkBx0Ljb1s3Rlw',
+    authDomain: 'nwitter-8bb8c.firebaseapp.com',
+    projectId: 'nwitter-8bb8c',
+    storageBucket: 'nwitter-8bb8c.appspot.com',
+    messagingSenderId: '475104752626',
+    appId: '1:475104752626:web:62a52b2d2d5a655ba4b16b',
+};
+
+export default firebase.initializeApp(firebaseConfig);
